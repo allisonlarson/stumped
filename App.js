2dee22ff2807dfca5f2fa8419871afee3e193890
Allie <allisonlarson91@gmail.com>
2017-05-07 12:38:50 -0400

    Add start component, organize app components
    Moves the app specific components into their own directory, for
    organization

--- App.js
+++ App.js
@@ -1,53 +1,11 @@
 import React from 'react';
-import Question from './question';
-import { StyleSheet, Text, View, Button, Alert } from 'react-native';
+import Start from './app/start';
 
 export default class App extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      isPlaying: false
-    }
-    this.handleOnPress = this.handleOnPress.bind(this)
-  }
-
-  handleOnPress() {
-    this.setState({ isPlaying: true })
-  }
-
   render() {
     return (
-      <View style={styles.container}>
-        { this.state.isPlaying ? (
-          <Question />
-          ) : (
-          <View>
-            <Text style={styles.welcomeHeader}>stumped</Text>
-
-            <Button
-              title="Get Started"
-              color='black'
-              onPress={this.handleOnPress}
-            />
-          </View>
-
-          )}
-
-        </View>
+      <Start />
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#b22222',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  welcomeHeader: {
-    color: 'white',
-    fontSize: 100
-  },
-});
 
---
+++ app/start.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import Question from './question';
+import { StyleSheet, Text, View, Button, Alert } from 'react-native';
+
+export default class Start extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      isPlaying: false
+    }
+    this.handleOnPress = this.handleOnPress.bind(this)
+  }
+
+  handleOnPress() {
+    this.setState({ isPlaying: true })
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        { this.state.isPlaying ? (
+          <Question />
+          ) : (
+          <View>
+            <Text style={styles.welcomeHeader}>stumped</Text>
+
+            <Button
+              title="Get Started"
+              color='black'
+              onPress={this.handleOnPress}
+            />
+          </View>
+
+          )}
+
+        </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#b22222',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  welcomeHeader: {
+    color: 'white',
+    fontSize: 100
+  },
+});
+
